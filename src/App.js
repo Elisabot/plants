26e9872f32de0b8a6606f710701f20b74928d7d0
Elisabot <elisabethmeyer@Earhart.local>
2018-04-09 10:58:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Plants</h1>
+        </header>
+        <p className="App-intro">
+          An application that reminds you to feed your plants.
+        </p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
