b4b22a5ecd61bdb97509d3d7773b86944e7c31d2
Elisabot <elisabeth.nimtzmeyer@gmail.com>
2018-09-28 18:26:48 -0700

    update material libs, peer dependencies, building again huzzah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.png'
+import 'typeface-roboto'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Plants</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to Plants</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           An application that reminds you to feed your plants.
         </p>
-        <div>
-        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
