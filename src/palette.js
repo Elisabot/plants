56b74fa917b8515cc26392d75cf44c6ef695beb8
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-01-07 11:03:05 -0800

    rename generic things and stub out some files

---
+++ src/palette.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import orange from '@material-ui/core/colors/orange'
+import green from '@material-ui/core/colors/green'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: orange
+  }
+})
